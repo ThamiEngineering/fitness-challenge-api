7f2c7777a3386cccaa7b01a9827d7ebc50e025c0
Thami <developer@thami.fr>
2025-07-23 08:23:41 +0200

    fix types

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,6 +1,11 @@
 declare global {
   namespace Express {
     interface Request {
+      user?: {
+        id: string;
+        role: string;
+        email?: string;
+      };
       rateLimit?: {
         resetTime?: Date;
         limit?: number;
